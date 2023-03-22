1b58d204643a2b031b2e2e44d63fa194d1bd9e33
Eryk Możdżeń <ermozdzen@gmail.com>
2023-03-22 01:41:43 +0100

    use libserial in usb-reader

--- software/sandbox/usb-reader/src/main.cpp
+++ software/sandbox/usb-reader/src/main.cpp
@@ -1,36 +1,53 @@
 #include "transfer.h"
 #include <iostream>
-#include "serial.h"
+#include <iomanip>
+#include <libserial/SerialStream.h>
 
 int main(int argc, char **argv) {
 	(void)argc;
 
-	Serial serial(argv[1]);
+	LibSerial::SerialStream serial(argv[1],
+		LibSerial::BaudRate::BAUD_115200,
+		LibSerial::CharacterSize::CHAR_SIZE_8,
+		LibSerial::FlowControl::FLOW_CONTROL_NONE,
+		LibSerial::Parity::PARITY_NONE,
+		LibSerial::StopBits::STOP_BITS_1
+	);
 
 	Transfer transfer;
 
-	while(1) {
+	const std::ios_base::fmtflags default_flags = std::cout.flags();
 
-		uint8_t buffer[16];
-		serial.readBytes(buffer, sizeof(buffer));
+	while(true) {
+        char byte;
+        serial.get(byte);
 
-		for(size_t i=0; i<sizeof(buffer); i++) {
-			const uint8_t byte = buffer[i];
+		transfer.consume(byte);
 
-			transfer.consume(byte);
+		Transfer::FrameRX rx;
+		if(transfer.receive(rx)) {
+			
+			std::cout.flags(default_flags);
+			std::cout << "[id: " << static_cast<int>(rx.id) << ", len: " << rx.length << "] ";
 
-			Transfer::FrameRX rx;
-			if(transfer.receive(rx)) {
+			switch(rx.id) {
+				case Transfer::LOG_DEBUG:
+				case Transfer::LOG_INFO:
+				case Transfer::LOG_WARNING:
+				case Transfer::LOG_ERROR: {
+					std::cout << std::string(reinterpret_cast<char *>(rx.payload), rx.length) << std::endl;
+				} break;
 
-				std::cout << "[id: " << static_cast<int>(rx.id) << ", len: " << rx.length << "] ";
+				case Transfer::TELEMETRY_ESTIMATION_ATTITUDE: {
+					struct Quaternion {float w, i, j, k;} q;
+					rx.getPayload(q);
 
-				std::string str;
-				for(size_t i=0; i<rx.length; i++)
-					str.push_back(static_cast<char>(rx.payload[i]));
+					std::cout << std::showpos << std::fixed << std::setprecision(5);
+					std::cout << q.w << " " << q.i << " " << q.j << " " << q.k << std::endl;
+				} break;
 
-				std::cout << str << std::endl;
+				default: break;
 			}
 		}
-		
-	}	
+    }
 }
