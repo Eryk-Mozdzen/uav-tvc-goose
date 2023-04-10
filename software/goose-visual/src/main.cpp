8c01075f1f4354a51e3ea3fc314eef99c574149f
Eryk Możdżeń <ermozdzen@gmail.com>
2023-04-10 12:57:50 +0200

    Telnet work on Qt

--- software/goose-visual/src/main.cpp
+++ software/goose-visual/src/main.cpp
@@ -2,6 +2,7 @@
 #include <QQmlApplicationEngine>
 
 #include "usb.h"
+#include "telnet.h"
 #include "printer_writer.h"
 
 int main(int argc, char *argv[]) {
@@ -10,13 +11,16 @@ int main(int argc, char *argv[]) {
 	QQmlApplicationEngine engine;
 	engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
-	USB usb("/dev/ttyUSB0");
-	//USB usb("/dev/ttyACM0");
+	USB usb("/dev/ttyACM0");
+	Telnet telnet("192.168.64.29", 23);
 
 	PrinterWriter printer_writer;
 
 	QObject::connect(&usb, &USB::receive, &printer_writer, &PrinterWriter::onReceive);
 	QObject::connect(&printer_writer, &PrinterWriter::onTransmit, &usb, &USB::transmit);
+
+	QObject::connect(&telnet, &Telnet::receive, &printer_writer, &PrinterWriter::onReceive);
+	QObject::connect(&printer_writer, &PrinterWriter::onTransmit, &telnet, &Telnet::transmit);
 
 	return app.exec();
 }
