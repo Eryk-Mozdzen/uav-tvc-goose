c2bbd2dcc95c94b7b6b08f0ac4bce153cfb69f8f
Eryk Możdżeń <ermozdzen@gmail.com>
2023-08-04 17:20:30 +0200

    add integral term to controller in FC

--- software/goose-fc/src/control/controller.cpp
+++ software/goose-fc/src/control/controller.cpp
@@ -12,10 +12,20 @@ void Controller::setProcessValue(const Matrix<X_NUM, 1> pv) {
     process_value = pv;
 }
 
-Matrix<Controller::U_NUM, 1> Controller::calculate() const {
+Matrix<Controller::U_NUM, 1> Controller::calculate() {
     const Matrix<X_NUM, 1> error = process_value - setpoint;
 
-    return operating_point - K*error;
+    error_integral +=G*error*dt;
+
+    Matrix<X_NUM + G_NUM, 1> e;
+    for(int i=0; i<X_NUM; i++) {
+        e(i, 0) = error(i, 0);
+    }
+    for(int i=0; i<G_NUM; i++) {
+        e(i + X_NUM, 0) = error_integral(i, 0);
+    }
+
+    return operating_point - K*e;
 }
 
 Matrix<Controller::X_NUM, 1> Controller::getSetpoint() const {
