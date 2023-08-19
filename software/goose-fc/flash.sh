c62340e3c6c3a5893326a92a3e602d6deaf02bf4
Eryk Możdżeń <ermozdzen@gmail.com>
2023-08-20 01:30:29 +0200

    add OTA flash option

--- software/goose-fc/flash.sh
+++ software/goose-fc/flash.sh
@@ -7,6 +7,15 @@ if [ "$type" == "stlink" ]; then
 	st-flash --connect-under-reset write $file 0x08000000
 elif [ "$type" == "dfu" ]; then
 	dfu-util -a 0 --dfuse-address 0x08000000:leave --download $file
+elif  [ "$type" == "ota" ]; then
+	arm-none-eabi-gdb -q $file << GDBSCRIPT
+		target extended-remote 192.168.102.29:2022
+		load
+		monitor reset
+		detach
+		quit
+GDBSCRIPT
+	echo ""
 else
 	echo "unknown program method"
 fi
