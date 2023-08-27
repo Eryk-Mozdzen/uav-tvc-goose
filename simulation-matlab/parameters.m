72dc14e482b95fbd1778d11b896a461ec53f8614
Eryk Możdżeń <ermozdzen@gmail.com>
2023-08-27 19:59:02 +0200

    tests of 8 thrust vanes

--- simulation-matlab/parameters.m
+++ simulation-matlab/parameters.m
@@ -17,14 +17,14 @@ C_La = 2*pi;
 % C_l = 0.5*C_La*A_fin/A_rot;
 C_l = 0.2;
 
-J_xx = 0.000834380;
-J_yy = 0.000827389;
-J_zz = 0.000150260;
+J_xx = 0.000834286;
+J_yy = 0.000826789;
+J_zz = 0.000192647;
 J_r = 0.000014703;
 
-m = 0.279;
-l = 0.06;
-r = 0.06575;
+m = 0.308;
+l = 0.04364 - 0.01325;
+r = 0.065;
 
 g = 9.81;
 
@@ -39,7 +39,9 @@ variance_dst = 0.000001;
 
 %% operating point
 
-alpha_0 = -K_tau/(4*r*C_l*K_t);
+%alpha_0 = -K_tau/(4*r*C_l*K_t);
+%alpha_0 = 0;
+alpha_0 = -(K_tau/(4*r*C_l*K_t) - deg2rad(9));
 throttle_0 = m*g/K_t;
 
 operating_point = [
@@ -55,7 +57,8 @@ fprintf('Nominalne wychylenie łopatek:      %4.1f deg\n', alpha_0*180/pi)
 
 %% linear model
 
-w_t0 = sqrt(m*g/K_w);
+% w_t0 = sqrt(m*g/K_w);
+w_t0 = 1050;
 F_t0 = K_t*throttle_0;
 
 A = [
@@ -75,19 +78,19 @@ A = [
 ];
 
 B = [
-     0,                0,                0,                0,                0;
-     0,                0,                0,                0,                0;
-     0,                0,                0,                0,                0;
-    -F_t0*C_l*l/J_xx,  0,                F_t0*C_l*l/J_xx,  0,                0;
-     0,               -F_t0*C_l*l/J_yy,  0,                F_t0*C_l*l/J_yy,  0;
-    -F_t0*C_l*r/J_zz, -F_t0*C_l*r/J_zz, -F_t0*C_l*r/J_zz, -F_t0*C_l*r/J_zz, -K_tau/J_zz;
-     0,                0,                0,                0,                0;
-     0,                0,                0,                0,                K_t/m;
+     0,                0,                0,                0,               0;
+     0,                0,                0,                0,               0;
+     0,                0,                0,                0,               0;
+    -F_t0*C_l*l/J_xx,  0,                F_t0*C_l*l/J_xx,  0,               0;
+     0,               -F_t0*C_l*l/J_yy,  0,                F_t0*C_l*l/J_yy, 0;
+    -F_t0*C_l*r/J_zz, -F_t0*C_l*r/J_zz, -F_t0*C_l*r/J_zz, -F_t0*C_l*r/J_zz, 0;
+     0,                0,                0,                0,               0;
+     0,                0,                0,                0,               K_t/m;
 
-     0,                0,                0,                0,                0;
-     0,                0,                0,                0,                0;
-     0,                0,                0,                0,                0;
-     0,                0,                0,                0,                0;
+     0,                0,                0,                0,               0;
+     0,                0,                0,                0,               0;
+     0,                0,                0,                0,               0;
+     0,                0,                0,                0,               0;
 ];
 
 G = [
@@ -100,27 +103,27 @@ G = [
 %% LQR regulator
 
 Q = diag([ ...
-    50;
-    50;
-    50;
-    10;
-    10;
-    10;
-    50;
-    10;
+    5;
+    5;
+    4;
+    1;
+    1;
+    2;
+    5;
+    3;
 
-    10;
-    10;
-    10;
-    20;
+    1;
+    1
+    1;
+    1;
 ]);
 
 R = diag([
+    300;
+    300;
+    300;
+    300;
     1000;
-    1000;
-    1000;
-    1000;
-    10000;
 ]);
 
 K = lqr(A, B, Q, R)
