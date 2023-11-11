6ad1284f7d53bb53734bd7d0605bc6df0147b2d9
Eryk Możdżeń <ermozdzen@gmail.com>
2023-11-11 18:13:32 +0100

    better K

--- software/goose-fc/controller.py
+++ software/goose-fc/controller.py
@@ -1,26 +1,35 @@
 import numpy as np
 import control
-import math
+import os
+from datetime import datetime
+from scipy.constants import g, pi
 
 K_t = 3.85756
 K_w = 0.0000014188
 K_tau = 0.04249
-C_l = 0.43
 
-J_xx = 0.000927790
-J_yy = 0.000918280
-J_zz = 0.000199379
+def fin_coefficient(R_rot, A_fin):
+    C_La = 2*pi
+    A_rot = pi*R_rot**2
+    return 0.5*C_La*A_fin/A_rot
+
+A_fin = 0.004203
+R_rot = 0.1
+C_l = fin_coefficient(R_rot, A_fin)
+
+J_xx = 0.000997943
+J_yy = 0.000988757
+J_zz = 0.000208322
 J_r = 0.000014703
 
-m = 0.332
-l = 0.04098 + 0.01225
+m = 0.337
+l = 0.03973 + 0.01225
 r = 0.0665
-alpha_s = math.radians(-5)
-g = 9.81
+alpha_s = np.radians(-5)
 
 alpha_0 = -K_tau/(4*r*C_l*K_t) - alpha_s
 throttle_0 = m*g/K_t
-w_t0 = 1050
+w_t0 = 1000 #np.sqrt(m*g/K_w)
 F_t0 = K_t*throttle_0
 
 operating_point = np.array([
@@ -52,10 +61,10 @@ B = np.array([
 ])
 
 G = np.array([
-	[1, 0, 0, 0, 0, 0, 0],
-	[0, 1, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 1, 0, 0],
-	[0, 0, 0, 0, 0, 1, 0]
+    [1, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0]
 ])
 
 Q = np.diag([
@@ -67,17 +76,17 @@ Q = np.diag([
     10,
     1,
 
-	50,
-	50,
-	20,
-	10
+    10,
+    10,
+    1,
+    10
 ])
 
 R = np.diag([
-    600,
-    600,
-    600,
-    600,
+    1000,
+    1000,
+    1000,
+    1000,
     1000
 ])
 
@@ -85,28 +94,31 @@ N = np.zeros((11, 5))
 
 K, _, _ = control.lqr(A, B, Q, R, N, integral_action=G)
 
-def write_int(file, num, name):
-	file.write('''constexpr int {0} = {1};\n'''.format(name, num))
+filepath = os.path.join(os.path.dirname(__file__), 'src/control/controller_params.h')
 
-def write_matrix(file, matrix, name, format='{: .4f}'):
-	file.write('''constexpr Matrix<{0}, {1}> {2} = {{\n'''.format(matrix.shape[0], matrix.shape[1], name))
-	for row in matrix:
-		row = [format.format(val) for val in row]
-		file.write('    ' + ', '.join(map(str, row)) + ',\n')
-	file.write('};\n\n')
+with open(filepath, 'w') as file:
+    def write_int(file, num, name):
+        file.write('''constexpr int {0} = {1};\n'''.format(name, num))
 
-with open('src/control/controller_params.h', 'w') as file:
-	file.write('// auto-generated file\n\n')
-	file.write('#pragma once\n\n')
-	file.write('#include <matrix.h>\n\n')
-	file.write('namespace controller {\n\n')
+    def write_matrix(file, matrix, name, format='{: .4f}'):
+        file.write('''constexpr Matrix<{0}, {1}> {2} = {{\n'''.format(matrix.shape[0], matrix.shape[1], name))
+        for row in matrix:
+            row = [format.format(val) for val in row]
+            file.write('    ' + ', '.join(map(str, row)) + ',\n')
+        file.write('};\n\n')
 
-	write_int(file, B.shape[1], 'dimU')
-	write_int(file, A.shape[0], 'dimX')
-	write_int(file, G.shape[0], 'dimG')
-	file.write('\n')
-	write_matrix(file, operating_point, 'operating_point')
-	write_matrix(file, K, 'K')
-	write_matrix(file, G, 'G', format='{:.0f}')
+    file.write('// auto-generated file\n')
+    file.write('// ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\n\n')
+    file.write('#pragma once\n\n')
+    file.write('#include <matrix.h>\n\n')
+    file.write('namespace controller {\n\n')
 
-	file.write('}\n')
+    write_int(file, B.shape[1], 'dimU')
+    write_int(file, A.shape[0], 'dimX')
+    write_int(file, G.shape[0], 'dimG')
+    file.write('\n')
+    write_matrix(file, operating_point, 'operating_point')
+    write_matrix(file, K, 'K')
+    write_matrix(file, G, 'G', format='{:.0f}')
+
+    file.write('}\n')
