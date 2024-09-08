291028652013727e20679faf99ac101d7ef4e387
Eryk Możdżeń <ermozdzen@gmail.com>
2024-09-08 18:08:03 +0200

    fix NVM

--- software/flight-controller/app/nvm.c
+++ software/flight-controller/app/nvm.c
@@ -7,13 +7,12 @@
 #define PAGE_BEGIN 0x0807E000
 #define PAGE_SIZE  0x2000
 
-static uint8_t page[PAGE_SIZE];
-
 void nvm_read(const uint32_t address, void *dest, const size_t len) {
     memcpy(dest, (void *)(PAGE_BEGIN + address), len);
 }
 
 void nvm_write(const uint32_t address, const void *src, const size_t len) {
+    uint8_t page[PAGE_SIZE];
     memcpy(page, (void *)PAGE_BEGIN, PAGE_SIZE);
     memcpy((void *)(page + address), src, len);
 
@@ -29,8 +28,8 @@ void nvm_write(const uint32_t address, const void *src, const size_t len) {
     uint32_t error;
     HAL_FLASHEx_Erase(&erase, &error);
 
-    for(uint32_t i=0; i<PAGE_SIZE; i +=128) {
-        HAL_FLASH_Program(FLASH_TYPEPROGRAM_BURST, PAGE_BEGIN + i, (uint32_t)(page + i));
+    for(uint32_t i=0; i<PAGE_SIZE; i +=16) {
+        HAL_FLASH_Program(FLASH_TYPEPROGRAM_QUADWORD, PAGE_BEGIN + i, (uint32_t)(page + i));
     }
 
     HAL_FLASH_Lock();
