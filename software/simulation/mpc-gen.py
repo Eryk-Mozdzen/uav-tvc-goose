5217e2d153bf150e3c7196c1149b2160a4ee2979
Eryk Możdżeń <ermozdzen@gmail.com>
2024-12-06 11:20:20 +0100

    MPC use just v instead of phi/theta/psi/thrust trajectories

--- software/simulation/mpc-gen.py
+++ software/simulation/mpc-gen.py
@@ -31,19 +31,8 @@ Z = cs.DM([
 QH = cs.DM.eye(NT)*100
 QZ = cs.DM.eye(4)*1
 
-def dynamics(x, u):
-    k0 = 2
-    k1 = 3
+def dynamics(x, v):
     m  = 0.518
-
-    v1 = u[8]  - k1*(x[10] - u[4]) - k0*(x[3] - u[0])
-    v2 = u[9]  - k1*(x[11] - u[5]) - k0*(x[4] - u[1])
-    v3 = u[10] - k1*(x[12] - u[6]) - k0*(x[5] - u[2])
-    v4 =                     u[7]  - k0*(x[6] - u[3])
-
-    ddx = (x[6]/m)*(cs.cos(x[3])*cs.sin(x[4])*cs.cos(x[5]) + cs.sin(x[3])*cs.sin(x[5]))
-    ddy = (x[6]/m)*(cs.cos(x[3])*cs.sin(x[4])*cs.sin(x[5]) - cs.sin(x[3])*cs.cos(x[5]))
-    ddz = (x[6]/m)*(cs.cos(x[3])*cs.cos(x[4])) - scipy.constants.g
 
     dx = cs.vcat([
         x[7],
@@ -52,58 +41,38 @@ def dynamics(x, u):
         x[10],
         x[11],
         x[12],
-        v4,
-        ddx,
-        ddy,
-        ddz,
-        v1,
-        v2,
-        v3,
+        v[3],
+        (x[6]/m)*(cs.cos(x[3])*cs.sin(x[4])*cs.cos(x[5]) + cs.sin(x[3])*cs.sin(x[5])),
+        (x[6]/m)*(cs.cos(x[3])*cs.sin(x[4])*cs.sin(x[5]) - cs.sin(x[3])*cs.cos(x[5])),
+        (x[6]/m)*(cs.cos(x[3])*cs.cos(x[4])) - scipy.constants.g,
+        v[0],
+        v[1],
+        v[2],
     ])
 
     return x + dx*T
 
-def control(u, uhd):
-    return cs.vcat([
-        u[0] + u[4]*T + 0.5*uhd[0]*T**2,
-        u[1] + u[5]*T + 0.5*uhd[1]*T**2,
-        u[2] + u[6]*T + 0.5*uhd[2]*T**2,
-        u[3] + uhd[3]*T,
-
-        u[4] + uhd[0]*T,
-        u[5] + uhd[1]*T,
-        u[6] + uhd[2]*T,
-        uhd[3],
-
-        uhd[0],
-        uhd[1],
-        uhd[2],
-    ])
-
 x_0 = cs.MX.sym('x_0', NX)
 x_tr = [cs.MX.sym('x_tr_' + str(i), NT) for i in range(HP)]
-uhd = [cs.MX.sym('uhd_' + str(i), NU) for i in range(HC)]
+v = [cs.MX.sym('v_' + str(i), NU) for i in range(HC)]
 
 cost = 0
 
 x = x_0
-u = cs.vcat([x[3], x[4], x[5], x[6], x[10], x[11], x[12], uhd[0][3], uhd[0][0], uhd[0][1], uhd[0][2]])
 for t in range(0, HC):
     dx = cs.mtimes([H, x]) - x_tr[t]
     dz = cs.mtimes([Z, x])
     cost +=cs.mtimes([dx.T, QH, dx])
     cost +=cs.mtimes([dz.T, QZ, dz])
-    x = dynamics(x, u)
-    u = control(u, uhd[t])
+    x = dynamics(x, v[t])
 for t in range(HC, HP):
     dx = cs.mtimes([H, x]) - x_tr[t]
     dz = cs.mtimes([Z, x])
     cost +=cs.mtimes([dx.T, QH, dx])
     cost +=cs.mtimes([dz.T, QZ, dz])
-    x = dynamics(x, u)
-    u = control(u, uhd[-1])
+    x = dynamics(x, v[-1])
 
-variables = cs.vertcat(*uhd)
+variables = cs.vertcat(*v)
 parameters = cs.vertcat(x_0, *x_tr)
 
 bounds = og.constraints.Rectangle(
--- software/simulation/mpc-test.py
+++ software/simulation/mpc-test.py
@@ -6,19 +6,8 @@ HP = 20
 HC = 5
 T = 0.05
 
-def dynamics(x, u):
-    k0 = 2
-    k1 = 3
+def dynamics(x, v):
     m  = 0.518
-
-    v1 = u[8]  - k1*(x[10] - u[4]) - k0*(x[3] - u[0])
-    v2 = u[9]  - k1*(x[11] - u[5]) - k0*(x[4] - u[1])
-    v3 = u[10] - k1*(x[12] - u[6]) - k0*(x[5] - u[2])
-    v4 =                     u[7]  - k0*(x[6] - u[3])
-
-    ddx = (x[6]/m)*(np.cos(x[3])*np.sin(x[4])*np.cos(x[5]) + np.sin(x[3])*np.sin(x[5]))
-    ddy = (x[6]/m)*(np.cos(x[3])*np.sin(x[4])*np.sin(x[5]) - np.sin(x[3])*np.cos(x[5]))
-    ddz = (x[6]/m)*(np.cos(x[3])*np.cos(x[4])) - scipy.constants.g
 
     dx = np.array([
         x[7],
@@ -27,33 +16,16 @@ def dynamics(x, u):
         x[10],
         x[11],
         x[12],
-        v4,
-        ddx,
-        ddy,
-        ddz,
-        v1,
-        v2,
-        v3,
+        v[3],
+        (x[6]/m)*(np.cos(x[3])*np.sin(x[4])*np.cos(x[5]) + np.sin(x[3])*np.sin(x[5])),
+        (x[6]/m)*(np.cos(x[3])*np.sin(x[4])*np.sin(x[5]) - np.sin(x[3])*np.cos(x[5])),
+        (x[6]/m)*(np.cos(x[3])*np.cos(x[4])) - scipy.constants.g,
+        v[0],
+        v[1],
+        v[2],
     ])
 
     return x + dx*T
-
-def control(u, uhd):
-    return np.array([
-        u[0] + u[4]*T + 0.5*uhd[0]*T**2,
-        u[1] + u[5]*T + 0.5*uhd[1]*T**2,
-        u[2] + u[6]*T + 0.5*uhd[2]*T**2,
-        u[3] + uhd[3]*T,
-
-        u[4] + uhd[0]*T,
-        u[5] + uhd[1]*T,
-        u[6] + uhd[2]*T,
-        uhd[3],
-
-        uhd[0],
-        uhd[1],
-        uhd[2],
-    ])
 
 data = []
 x = np.array([0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0])
@@ -101,30 +73,25 @@ for i in range(500):
     response = mng.call(x.tolist() + x_tr.flatten().tolist())
 
     if response.is_ok():
-        uhd = np.array(response.get().solution).reshape(HC, 4)
+        v = np.array(response.get().solution).reshape(HC, 4)
     else:
         print(response.get().message)
 
     x_pred = np.zeros((HP+1, 13))
     x_pred[0] = x
-    u = np.hstack([x[3], x[4], x[5], x[6], x[10], x[11], x[12], uhd[0][3], uhd[0][0], uhd[0][1], uhd[0][2]])
     for k in range(0, HC):
-        x_pred[k+1] = dynamics(x_pred[k], u)
-        u = control(u, uhd[k])
+        x_pred[k+1] = dynamics(x_pred[k], v[k])
     for k in range(HC, HP):
-        x_pred[k+1] = dynamics(x_pred[k], u)
-        u = control(u, uhd[-1])
-
-    u = np.hstack([x[3], x[4], x[5], x[6], x[10], x[11], x[12], uhd[0][3], uhd[0][0], uhd[0][1], uhd[0][2]])
+        x_pred[k+1] = dynamics(x_pred[k], v[-1])
 
     data.append({
         'x': x,
         'x_tr': x_tr,
-        'u': u,
+        'v': v[0],
         'x_pred': x_pred
     })
 
-    x = dynamics(x, u)
+    x = dynamics(x, v[0])
 
 mng.kill()
 
@@ -160,20 +127,18 @@ ax11.set_xlabel('t [s]')
 ax11.grid()
 
 dir_xt = ax00.quiver(0, 0, 1, 0, angles='xy', scale_units='xy', scale=2, color='k', zorder=1)
-line_tr, = ax00.plot([], [], 'k--', zorder=1)
-line_pr, = ax00.plot([], [], 'b--', zorder=2)
+line_tr, = ax00.plot([], [], 'k--', zorder=1, label='xy trajectory')
+line_pr, = ax00.plot([], [], '--', zorder=2, label='xy')
 dir_x = ax00.quiver(0, 0, 1, 0, angles='xy', scale_units='xy', scale=2, color='r', zorder=3)
 
 line_z, = ax10.plot([], [], label='z')
 line_zt, = ax10.plot([], [], 'k--', label='z trajectory')
 
 line_phi, = ax01.plot([], [], label='phi')
-line_phit, = ax01.plot([], [], 'k--', label='phi trajectory')
 line_theta, = ax01.plot([], [], label='theta')
-line_thetat, = ax01.plot([], [], 'k--', label='theta trajectory')
 line_psi, = ax11.plot([], [], label='psi')
-line_psit, = ax11.plot([], [], 'k--', label='psi trajectory')
 
+ax00.legend()
 ax10.legend()
 ax01.legend()
 ax11.legend()
@@ -184,12 +149,12 @@ ax11.set_xlim(0, len(data)*T)
 
 x = []
 tr = []
-u = []
+v = []
 
 def init():
     x.clear()
     tr.clear()
-    u.clear()
+    v.clear()
     dir_xt.set_offsets([0, 0])
     dir_xt.set_UVC(1, 0)
     dir_x.set_offsets([0, 0])
@@ -199,19 +164,16 @@ def init():
     line_z.set_data([], [])
     line_zt.set_data([], [])
     line_phi.set_data([], [])
-    line_phit.set_data([], [])
     line_theta.set_data([], [])
-    line_thetat.set_data([], [])
     line_psi.set_data([], [])
-    line_psit.set_data([], [])
-    return dir_xt, dir_x, line_tr, line_pr, line_z, line_zt, line_phi, line_phit, line_theta, line_thetat, line_psi, line_psit,
+    return dir_xt, dir_x, line_tr, line_pr, line_z, line_zt, line_phi, line_theta, line_psi,
 
 def update(frame):
     d = data[frame]
 
     x.append((frame*T, d['x']))
     tr.append((frame*T, d['x_tr'][0]))
-    u.append((frame*T, d['u']))
+    v.append((frame*T, d['v']))
 
     dir_xt.set_offsets([
         d['x_tr'][0][0],
@@ -251,28 +213,16 @@ def update(frame):
         [i[0] for i in x],
         [np.rad2deg(i[1][3]) for i in x],
     )
-    line_phit.set_data(
-        [i[0] for i in u],
-        [np.rad2deg(i[1][0]) for i in u],
-    )
     line_theta.set_data(
         [i[0] for i in x],
         [np.rad2deg(i[1][4]) for i in x],
-    )
-    line_thetat.set_data(
-        [i[0] for i in u],
-        [np.rad2deg(i[1][1]) for i in u],
     )
     line_psi.set_data(
         [i[0] for i in x],
         [np.rad2deg(i[1][5]) for i in x],
     )
-    line_psit.set_data(
-        [i[0] for i in u],
-        [np.rad2deg(i[1][2]) for i in u],
-    )
 
-    return dir_xt, dir_x, line_tr, line_pr, line_z, line_zt, line_phi, line_phit, line_theta, line_thetat, line_psi, line_psit,
+    return dir_xt, dir_x, line_tr, line_pr, line_z, line_zt, line_phi, line_theta, line_psi,
 
 anim = FuncAnimation(
     fig=fig,
