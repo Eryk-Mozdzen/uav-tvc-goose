76da9133537c17b62293e6c33f8403fc5b9481b9
Eryk Możdżeń <ermozdzen@gmail.com>
2024-12-10 17:03:56 +0100

    drake + MPC integration, oscillations

--- software/simulation/src/controllers/MPC.h
+++ software/simulation/src/controllers/MPC.h
@@ -3,36 +3,26 @@
 #include <drake/systems/framework/leaf_system.h>
 #include <drake/systems/framework/diagram.h>
 
-class MPCFL : public drake::systems::Diagram<double> {
+class MPC : public drake::systems::Diagram<double> {
     static constexpr int HP = 20;
     static constexpr int HC = 5;
     static constexpr double T = 0.05;
 
-    static constexpr int NX = 13;
+    static constexpr int NX = 14;
     static constexpr int NT = 8;
     static constexpr int NV = 4;
     static constexpr int NU = 4;
 
-    class MPC : public drake::systems::LeafSystem<double> {
+    class PositionController : public drake::systems::LeafSystem<double> {
         void update(const drake::systems::Context<double> &context, drake::systems::DiscreteValues<double> *updates) const;
         void eval(const drake::systems::Context<double> &context, drake::systems::BasicVector<double> *output) const;
 
     public:
-        MPC();
-    };
-
-    class FeedbackLinearization : public drake::systems::LeafSystem<double> {
-        static constexpr double k0 = 2;
-        static constexpr double k1 = 3;
-
-        void eval(const drake::systems::Context<double> &context, drake::systems::BasicVector<double> *output) const;
-
-    public:
-        FeedbackLinearization();
+        PositionController();
     };
 
 public:
-    MPCFL();
+    MPC();
 
     const drake::systems::InputPort<double> & get_state_input_port() const;
     const drake::systems::InputPort<double> & get_trajectory_input_port() const;
--- software/simulation/src/main.cpp
+++ software/simulation/src/main.cpp
@@ -1,54 +1,25 @@
 #include <drake/systems/framework/diagram_builder.h>
 #include <drake/systems/analysis/simulator.h>
 
-//#include "Circle.h"
-//#include "Lemniscate.h"
-//#include "Simple3.h"
-//#include "MPCFL.h"
+#include "Lemniscate.h"
+#include "MPC.h"
 #include "Plant.h"
-#include "PlantFeedbackLinearization.h"
 #include "Sink.h"
-
-class Controller : public drake::systems::LeafSystem<double> {
-    void eval(const drake::systems::Context<double> &context, drake::systems::BasicVector<double> *output) const {
-		const Eigen::Vector<double, 14> x = this->GetInputPort("x").Eval(context);
-
-		const double t = context.get_time();
-
-		const double y0 = +0.5*sin(2*3.1415*t);
-		const double y1 = +0.5*cos(2*3.1415*t)*2*3.1415;
-		const double y2 = -0.5*sin(2*3.1415*t)*2*3.1415*2*3.1415;
-
-		const double k0 = 2;
-		const double k1 = 3;
-
-		output->SetAtIndex(0, y2 - k1*(x[10] - y1) - k0*(x[3] - y0));
-		output->SetAtIndex(1, y2 - k1*(x[11] - y1) - k0*(x[4] - y0));
-		output->SetAtIndex(2, y2 - k1*(x[12] - y1) - k0*(x[5] - y0));
-		output->SetAtIndex(3,                  y1  - k0*(Plant::K_f*x[13]*x[13] - (y0 + 1)));
-	}
-
-public:
-    Controller() {
-		this->DeclareVectorInputPort("x", 14);
-		this->DeclareVectorOutputPort("v", 4, &Controller::eval);
-	}
-};
 
 int main() {
 	drake::systems::DiagramBuilder<double> builder;
 
-	auto control = builder.AddSystem<Controller>();
-	auto linear = builder.AddSystem<PlantFeedbackLinearization>();
+	auto generator = builder.AddSystem<Lemniscate>(2, 10);
+	auto controller = builder.AddSystem<MPC>();
 	auto plant = builder.AddSystem<Plant>();
 	auto sink = builder.AddSystem<Sink>();
 
-	builder.Connect(control->GetOutputPort("v"), linear->GetInputPort("v"));
-	builder.Connect(linear->GetOutputPort("u"), plant->GetInputPort("u"));
-	builder.Connect(plant->GetOutputPort("x"), linear->GetInputPort("x"));
-	builder.Connect(plant->GetOutputPort("x"), control->GetInputPort("x"));
+	builder.Connect(generator->get_output_port(), controller->get_trajectory_input_port());
+	builder.Connect(plant->GetOutputPort("x"), controller->get_state_input_port());
+	builder.Connect(controller->get_control_output_port(), plant->GetInputPort("u"));
 
 	sink->Connect(&builder, plant->GetOutputPort("x"), "x,y,z,phi,theta,psi,beta,dx,dy,dz,dphi,dtheta,dpsi,dbeta");
+	sink->Connect(&builder, controller->get_control_output_port(), "ur,a1,a2,a3");
 
 	auto diagram = builder.Build();
 
@@ -57,5 +28,5 @@ int main() {
 	//simulator.get_mutable_integrator().set_fixed_step_mode(true);
 	//simulator.get_mutable_integrator().set_throw_on_minimum_step_size_violation(false);
 	//simulator.get_mutable_integrator().set_maximum_step_size(0.01);
-	simulator.AdvanceTo(30);
+	simulator.AdvanceTo(10);
 }
